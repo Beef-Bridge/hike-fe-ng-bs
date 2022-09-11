12c058c34480d0d88e911de0f32d0cd26381634a
Nikoquattro <nikoquattro@gmail.com>
2022-09-12 00:15:56 +0200

    creation architecture du projet (modules share, ui et components header, footer, nav et ui)

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { UiComponent } from './containers/ui/ui.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    UiComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiModule { }
